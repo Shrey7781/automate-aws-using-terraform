45b7fda8cb510de7bf8fe1ed0a22f4f54e05cb19
Shrey7781 <shreeyansh.saxena.dev@gmail.com>
2026-01-14 15:07:57 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "aws"{
+    region = "ap-south-1"
+}
+
+variable "subnet_cidr_block" {
+  description = "subnet cidr block"
+  default = "10.0.10.0/24"
+  type = string ///list(string)
+}
+variable "vpc_cidr_block"{
+    description = "vpc cidr block"
+}
+variable "environment"{
+    description = "deployment environment"
+}
+
+resource "aws_vpc" "development-vpc"{
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: var.environment,
+        vpc-env: "dev"
+    }
+}
+
+
+resource "aws_subnet" "dev-subnet-1"{
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.subnet_cidr_block
+    
+    availability_zone = "ap-south-1a"
+    tags = {
+        Name: "subnet-1-dev"
+    }
+}
+
+data "aws_vpc" "existing-vpc" {
+    default = true    
+}
+resource "aws_subnet" "dev-subnet-2"{
+    vpc_id = data.aws_vpc.existing-vpc.id
+    cidr_block = "172.31.48.0/20"
+    availability_zone = "ap-south-1a"
+    tags = {
+        Name: "subnet-2-default"
+    }
+}
+
+output "dev-vpc-id"{
+    value = aws_vpc.development-vpc.id
+}
+output "dev-subnet-id"{
+    value = aws_subnet.dev-subnet-1.id
+}
